c19f95ab9c63c94e37b25cff2bccbd2418faf67e
신예솔 <74286242+sssol99@users.noreply.github.com>
2023-01-05 16:53:46 +0900

    [Silver I] Title: 넴모넴모 (Easy), Time: 472 ms, Memory: 12404 KB -BaekjoonHub

---
+++ 백준/Silver/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+	<문제 조건>
+	 세로 : M
+	 가로 : N
+ *N과 M은 1이상 25 이하
+ *격자판 위에서 넴모가 나오지 않는 모든 경우의 수를 출력한다
+
+	<넴모>
+	2*2 사각형을 이루는 배치
+
+	<힌트>
+	2*2격자판에 넴모를 배치하는 방법은 모든 배치 경우의 수 - 네 칸 모두에 넴모가 올라가 있는 경우
+
+	<풀이 방법>
+	 왼쪽, 왼쪽 대각선, 위쪽을 체크해서 넴모인지 확인
+ */
+public class Main {
+
+	static int res; // 답 저장할 배열
+	static int I; //열
+	static int J; //행
+	static int DI[] = {0,-1,-1}; // 넴모체크 I
+	static int DJ[] = {-1,-1,0}; // 넴모체크 J
+	static boolean[][] arr;
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+
+		I = Integer.parseInt(stk.nextToken()); // 행
+		J = Integer.parseInt(stk.nextToken()); // 열	
+
+		arr = new boolean[I+1][J+1]; // 배열 초기화
+
+		DFS(0);
+		System.out.println(res);
+	}
+
+	public static void DFS(int cnt) {
+		if(cnt==I*J) {
+			res++;
+			return;
+		}
+
+		int nowI = cnt / J + 1;
+		int nowJ = cnt % J + 1;
+
+		//넴모가 아닌 경우
+		if(!arr[nowI][nowJ-1] | !arr[nowI-1][nowJ-1] | !arr[nowI-1][nowJ]) {
+			arr[nowI][nowJ] = true;
+			DFS(cnt+1);
+		}
+		arr[nowI][nowJ] = false;
+		DFS(cnt+1);
+	}
+}
