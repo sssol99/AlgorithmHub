e62ec4ad3b6725496802a014ac7052a138801943
신예솔 <74286242+sssol99@users.noreply.github.com>
2023-01-20 17:04:01 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 112 ms, Memory: 12684 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int tk = Integer.parseInt(br.readLine());
+		int arr[][] = new int[10001][3];
+
+		//1~3까지 초기화
+		for(int i = 1 ; i <= 3 ; i++){
+			for(int j = 0 ; j < i ; j++){
+				arr[i][j] = 1;
+			}
+		}
+
+		//DP 시작
+		for(int i = 4 ; i < 10001 ; i++){
+			arr[i][0] = 1;
+			arr[i][1] = arr[i-2][1] + arr[i-2][0];
+			arr[i][2] = arr[i-3][0] + arr[i-3][1] + arr[i-3][2];
+		}
+
+		for(int t = 0; t < tk ; t++){
+			int N = Integer.parseInt(br.readLine());
+
+			System.out.println(arr[N][0]+arr[N][1]+arr[N][2]);
+		}
+	}
+}
